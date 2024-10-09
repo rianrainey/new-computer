9629b19c2f95e5d19fd6b3880f3a9914eefa112c
Rian Rainey <rianrainey@gmail.com>
2024-10-09 09:42:36 -0400

    Don't install denylist apps

--- apps.sh
+++ apps.sh
@@ -1,8 +1,18 @@
+# Check if Homebrew is installed, install if not
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Confirmed Homebrew is installed"
+fi
+
 echo "Starting brew app install..."
 
 brew_casks=(
   1password
   alfred
+  chatgpt 
   cursor
   dash
   dbeaver-community
@@ -10,6 +20,7 @@ brew_casks=(
   firefox
   flux
   iterm2
+  logi-options+ # require reboot
   loom
   notion
   postman
@@ -20,7 +31,24 @@ brew_casks=(
   # Add more casks as needed
 )
 
+# Casks you don't want to install at client
+client_denylist=(
+  1password # they already use this
+  chatgpt 
+  cursor # not allowed
+  fantastical # not allowed
+)
+
+# Subtract client_denylist from brew_casks
+filtered_brew_casks=()
 for cask in "${brew_casks[@]}"; do
+  if [[ ! " ${client_denylist[@]} " =~ " ${cask} " ]]; then
+    filtered_brew_casks+=("$cask")
+  fi
+done
+
+# Loop through the filtered list
+for cask in "${filtered_brew_casks[@]}"; do
   if brew list --cask "$cask" &>/dev/null; then
     echo "$cask is already installed"
   else
