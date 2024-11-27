c333aa54bd844c740e1c43cfaf9a29d2366290a5
Rian Rainey <rianrainey@gmail.com>
2024-11-27 06:37:12 -0500

    Add default languages for asdf

---
+++ asdf.sh
@@ -0,0 +1,13 @@
+# Ruby
+asdf plugin add ruby
+asdf install ruby latest
+asdf global ruby latest
+
+# Node.js
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs latest
+asdf global nodejs latest
+
+# Python
+asdf plugin add python
+asdf install python latest
